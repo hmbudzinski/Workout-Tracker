f6cba4d92901761662831e0807c2c33268f40e6f
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-03-15 13:46:12 -0600

    routing

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./models/model.js");
+const User = require("./models");
 
 const app = express();
 
@@ -15,7 +16,26 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(_dirname + "/public/index.html"))
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(_dirname + "/public/exercise.html"))
+})
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(_dirname + "/public/stats.html"))
+})
+
+app.get("/api/workouts", (req, res) => {
+    db.Workouts.find()
+        .then(data => {
+            res.json(data);
+        });
+});
 
 app.post("/submit", ({ body }, res) => {
     const user = new User(body);
